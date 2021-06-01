aa85e40c9980ee15523f380034df79fce6dad594
vish4vision <vish4vision@gmail.com>
2021-06-01 12:32:38 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#003C5A',
+        blueDefault: '#0087CA',
+        blueHover: '#0070A8',
+        greenDefault: '#3CC88C',
+        grayDefault: {
+          100: '#F7F7F7',
+          200: '#B0B0B0',
+          300: '#4A4A4A',
+          400: '#DBE3E7',
+        }
+      },
+      fontFamily: {
+        body: ['Montserrat']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
